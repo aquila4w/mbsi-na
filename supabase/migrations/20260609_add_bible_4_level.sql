e743c011f9c5e1257f379775e157ebe99ca9af83
aquila4w <leomarquez@gmail.com>
2026-06-09 12:36:55 -0700

    Replace predictable sequential UUIDs with random UUIDs
    - Update seed file to use random UUIDs
    - Update bible_4 migration to use random UUIDs
    - Add migration to reassign existing DB rows to random UUIDs

--- supabase/migrations/20260609_add_bible_4_level.sql
+++ supabase/migrations/20260609_add_bible_4_level.sql
@@ -25,5 +25,5 @@ SET target_level_id = l.next_level_id
 FROM student_metrics_students s
 JOIN mbsi_levels l ON l.id = s.current_level_id
 WHERE d.student_id = s.id
-  AND d.session_id = 'b0000001-0000-0000-0000-000000000001'
-  AND s.id::text LIKE 'a0000001-0000-0000-0000-%';
+  AND d.session_id = '3394bbcd-c038-403d-a34d-65a40c7fa014'
+  AND s.id::text IN ('75d65649-e6ea-48f5-95f9-c8a7489cacbf','175763ac-6988-4341-8285-221c048a3657','de1882f4-8d65-47c7-9c4d-401ec810ca75','5a7d3133-ed34-4d92-95c3-2136ee73d73b','9ad930d0-7ba6-4759-8583-7deb64ee5ada','1437b0e3-fbb6-4f70-87a9-c1ee73eaabf4','4008a653-099f-4957-925c-9cab5588bb0e','1fe3c0af-4a46-41e5-b5ae-e793f0052480','230292f0-d27b-4a0c-9eba-e09a1ff35271','a45901bf-ffb5-4ccc-816d-2ce5a97e8855','22f8ff61-9489-4e2d-bc44-a73d984559b1','5434e64f-539c-49f1-88a2-1e06e211e13e','860150c0-01cc-4167-a43c-ee5e799b71eb','ed776323-9406-42f9-a545-45e33f42fd3a','46defcd5-196d-4edd-ab1c-6de089b96edc','f7bfc0e0-38f4-49d4-8290-f18952e50b24','330eb8f4-7da3-4c2c-9931-b35bbd029f86','a3f394ab-07d5-4780-b358-85044cfbbee3','58d03703-a257-4111-a61e-01ca0df0c557','250d0880-46c2-4672-b038-b8f5a1e26b8d','8adf9f24-a648-4d73-827e-9e9dabde063e','bf81a79c-67d5-46b4-9556-ed3eb2158d3e','efc4dc77-0abb-471a-a963-435ae2d5d1e8','d9d3b6c5-b148-4fe6-a36d-bb4dd5a143ec','f7672672-4f02-447f-af73-ddfe21641770','248b1d5f-4156-4596-bd06-a8f6ad00c83d','b8beeaa7-7cc7-450e-b5e0-4728b56a1882');
